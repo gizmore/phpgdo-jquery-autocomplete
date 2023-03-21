71e97c5a08708697f29cd283bcbef879d5f4f848
gizmore <gizmore@wechall.net>
2023-03-21 19:22:36 +0100

    '7.0.2-r1765 Hailing Hermelin'

--- Module_JQueryAutocomplete.php
+++ Module_JQueryAutocomplete.php
@@ -5,43 +5,44 @@ use GDO\Core\GDO_Module;
 
 /**
  * EasyAutocomplete wrapper module.
- * 
+ *
  * @link https://github.com/pawelczak/EasyAutocomplete
- * 
+ *
  * @author gizmore
  * @version 7.0.1
  * @since 6.10.0
  */
 final class Module_JQueryAutocomplete extends GDO_Module
 {
-    public int $priority = 45;
-    public string $license = 'MIT';
-    
-    public function getDependencies() : array
-    {
-    	return ['JQuery'];
-    }
-    
-    public function thirdPartyFolders() : array
-    {
-    	return ['EasyAutocomplete/'];
-    }
-    
-    public function getLicenseFilenames() : array
-    {
-    	return [
-    		'MIT.LICENSE',
-    		'EasyAutocomplete/LICENSE.txt',
-    	];
-    }
-    
-    public function onIncludeScripts() : void
-    {
-        $min = $this->cfgMinAppend();
-        $this->addCSS("EasyAutocomplete/dist/easy-autocomplete{$min}.css");
-        $this->addCSS("EasyAutocomplete/dist/easy-autocomplete.themes{$min}.css");
-        $this->addJS("EasyAutocomplete/dist/jquery.easy-autocomplete{$min}.js");
-        $this->addJS("js/gdo7-easy-autocomplete.js");
-    }
-    
+
+	public int $priority = 45;
+	public string $license = 'MIT';
+
+	public function getDependencies(): array
+	{
+		return ['JQuery'];
+	}
+
+	public function thirdPartyFolders(): array
+	{
+		return ['EasyAutocomplete/'];
+	}
+
+	public function getLicenseFilenames(): array
+	{
+		return [
+			'MIT.LICENSE',
+			'EasyAutocomplete/LICENSE.txt',
+		];
+	}
+
+	public function onIncludeScripts(): void
+	{
+		$min = $this->cfgMinAppend();
+		$this->addCSS("EasyAutocomplete/dist/easy-autocomplete{$min}.css");
+		$this->addCSS("EasyAutocomplete/dist/easy-autocomplete.themes{$min}.css");
+		$this->addJS("EasyAutocomplete/dist/jquery.easy-autocomplete{$min}.js");
+		$this->addJS('js/gdo7-easy-autocomplete.js');
+	}
+
 }
--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 
 Easy Autocomplete wrapper module for the GDOv7 framework.
 
-
 ## phpgdo-jquery-autocomplete: Installation
 
 Follow the GDOv7
 [installation guide](https://github.com/gizmore/phpgdo ).
-
 
 ## phpgdo-jquery-autocomplete: Configuration
 
